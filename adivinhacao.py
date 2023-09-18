8ddb47bd04b34d63a328aff827e775f9e8ea5c9c
Thiago Melo <es.thg.melo@gmail.com>
2023-09-18 07:09:44 -0300

    feat: condicao elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,9 +10,15 @@ print('Você digitou ', chute_str)
 
 chute = int(chute_str)
 
-if numero_secreto == chute:
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if acertou:
     print('Você acertou!')
 else:
-    print('Você errou!')
-
+    if maior:
+        print('Você errou! O seu chute foi maior que o número secreto.')
+    elif menor:
+        print('Você errou! Seu chute foi menor que o número secreto')
 print('\nFim do jogo')
