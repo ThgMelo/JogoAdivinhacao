19c8f11c95b5e25bf49ea39f5891acef4f3e6661
Thiago Melo <es.thg.melo@gmail.com>
2023-09-18 07:26:04 -0300

    feat: laço com while

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Adivinhação - Python
+![Python](https://img.shields.io/badge/python-3.11.5-blue) 
 Meu primeiro projeto na linguagem python
--- adivinhacao.py
+++ adivinhacao.py
@@ -4,21 +4,27 @@ print('*********************************\n')
 
 numero_secreto = 42
 
-chute_str = int(input('Digite o seu numero: '))
+total_de_tentativas = 3
+rodada = 1
 
-print('Você digitou ', chute_str)
+while (rodada <= total_de_tentativas):
+    print(f'Tentativa {rodada} de {total_de_tentativas}')
+    chute_str = int(input('Digite o seu numero: '))
+    print('Você digitou ', chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    if acertou:
+        print('Você acertou!')
+    else:
+        if maior:
+            print('Você errou! O seu chute foi maior que o número secreto.')
+        elif menor:
+            print('Você errou! Seu chute foi menor que o número secreto')
 
-if acertou:
-    print('Você acertou!')
-else:
-    if maior:
-        print('Você errou! O seu chute foi maior que o número secreto.')
-    elif menor:
-        print('Você errou! Seu chute foi menor que o número secreto')
+    rodada += 1
+
 print('\nFim do jogo')
