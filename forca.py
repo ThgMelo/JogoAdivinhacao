5db7ba14d7968acec17401007e4733e35a6e913e
Thiago Melo <es.thg.melo@gmail.com>
2023-09-19 12:38:50 -0300

    feat: game loop

--- forca.py
+++ forca.py
@@ -1,8 +1,15 @@
-
 def jogar():
     print('**************************')
     print('Bem vindo ao jogo de Forca!')
     print('**************************\n')
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
 
     print('\nFim do jogo')
 
