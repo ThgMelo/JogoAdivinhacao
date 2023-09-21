9629bfc65603d5895697911afd50b1a229cc30ae
Thiago Melo <es.thg.melo@gmail.com>
2023-09-21 07:24:17 -0300

    feat: Estipulando tentativas de erros

--- forca.py
+++ forca.py
@@ -3,26 +3,37 @@ def jogar():
     print('Bem vindo ao jogo de Forca!')
     print('**************************\n')
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_",]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input('Qual letra?: ')
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index += 1
+        chute = chute.strip().upper()
 
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if acertou:
+        print('Você ganhou!')
+    else:
+        print('Você perdeu!')
     print('\nFim do jogo')
 
 if __name__ == '__main__':
